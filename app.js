5337bd0160a9c1a1901951aea33dd50b2fa65068
Dasun <w.piyumal2319@gmail.com>
2024-08-22 22:07:45 +0530

    removed body parser

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 //jshint esversion:6
 
 const express = require("express");
-const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
 
@@ -13,7 +12,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 app.use(express.static("public"));
 
 let posts = [];
